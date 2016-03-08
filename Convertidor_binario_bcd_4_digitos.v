d6b807440b97c9d7a6d880cf0f78c56519fad36e
Daniel Rojas Chacon <daniroj1012@gmail.com>
2016-03-07 22:11:46 -0600

    Added files via upload

---
+++ Convertidor_binario_bcd_4_digitos.v
@@ -0,0 +1,50 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date:    19:28:25 03/06/2016 
+// Design Name: 
+// Module Name:    Convertidor_binario_bcd_4_digitos 
+// Project Name: 
+// Target Devices: 
+// Tool versions: 
+// Description: 
+//
+// Dependencies: 
+//
+// Revision: 
+// Revision 0.01 - File Created
+// Additional Comments: 
+//
+//////////////////////////////////////////////////////////////////////////////////
+module Convertidor_binario_bcd_4_digitos(dato,unidades,decenas,centenas,millares
+    );
+	 input wire [9:0] dato;
+	 output reg [3:0] unidades,decenas,centenas,millares;
+	 reg [25:0] auxiliar;
+	 integer i;
+	 always @(dato)begin
+	 auxiliar[25:10]=0;
+	 auxiliar[9:0]=dato;
+	 for(i=0;i<10;i=i+1)begin
+	      if (auxiliar[13:10] >= 5)
+            auxiliar[13:10] = auxiliar[13:10] + 3;
+            
+         if (auxiliar[17:14] >= 5)
+            auxiliar[17:14] = auxiliar[17:14] + 3;
+            
+         if (auxiliar[21:18] >= 5)
+            auxiliar[21:18] = auxiliar[21:18] + 3;
+				
+			if (auxiliar[25:22] >= 5)
+            auxiliar[25:22] = auxiliar[25:22] + 3;
+         auxiliar = auxiliar << 1;
+	 end
+	 unidades= auxiliar[13:10];
+	 decenas= auxiliar[17:14];
+	 centenas= auxiliar[21:18];
+	 millares= auxiliar[25:22];
+	 
+	 end
+endmodule
---
+++ deco_7_segmentos.v
@@ -0,0 +1,40 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date:    21:56:34 03/06/2016 
+// Design Name: 
+// Module Name:    deco_7_segmentos 
+// Project Name: 
+// Target Devices: 
+// Tool versions: 
+// Description: 
+//
+// Dependencies: 
+//
+// Revision: 
+// Revision 0.01 - File Created
+// Additional Comments: 
+//
+//////////////////////////////////////////////////////////////////////////////////
+module deco_7_segmentos(codigo,display
+    );
+input wire [3:0] codigo;
+output reg [6:0] display;
+always @(codigo) begin
+case (codigo)       
+  4'b0000: display = 7'b1111110;
+  4'b0001: display = 7'b0110000;
+  4'b0010: display = 7'b1101101;
+  4'b0011: display = 7'b1111001;
+  4'b0100: display = 7'b0110011;
+  4'b0101: display = 7'b1011011;
+  4'b0110: display = 7'b1011111;
+  4'b0111: display = 7'b1110000;
+  4'b1000: display = 7'b1111111;
+  4'b1001: display = 7'b1111011;
+  default: display = 7'b0000000;
+endcase
+end
+endmodule
